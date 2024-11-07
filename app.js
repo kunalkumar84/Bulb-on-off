8ed27142c9fab16d24a01d8e21c44170acb5f29d
kunalkumar84 <kunal896079@gmail.com>
2024-11-07 10:52:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+document.getElementById("img").src = "images.jpg/of.png";
+document.getElementById("text").innerHTML = "Off";
+
+
+
+
+
+function btnon() {
+
+    document.getElementById("img").src = "images.jpg/on.png";
+    document.getElementById("text").innerHTML = "ON"
+
+
+
+
+}
+
+function btnof() {
+    document.getElementById("img").src = "images.jpg/of.png";
+    document.getElementById("text").innerHTML = "Off"
+
+
+
+}
